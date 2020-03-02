5479b6489d305cfdfc80485bf7e668f9cfc83721
yerzhanin <yerzhan.int@gmail.com>
2020-03-02 16:22:49 +0600

    Added loop and fix if input is string

--- Hi_Lo.py
+++ Hi_Lo.py
@@ -2,8 +2,15 @@ import random
 
 target_number = random.randrange(0, 10)
 print(target_number)
-user_number = int(input("Enter the number: "))
-if target_number == user_number:
-    print("You win!")
-else:
-    print("You lose!")
+while True:
+    try:
+     user_number = int(input("Enter the number: "))
+    except:
+        print("Incorrect input")
+    if target_number == user_number:
+        print("You win!")
+        break
+    elif target_number > user_number:
+        print("take a bit higher")
+    elif target_number < user_number:
+        print("take a bit lower")
